fe12a51ba75b0badd3a6cf39dd5e33ddd0696a43
Eric Hernandez <eands9@yahoo.com>
2019-02-12 13:43:37 -0600

    Initial Commit

---
+++ ehfirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ehfirebase
+//
+//  Created by Eric Hernandez on 2/12/19.
+//  Copyright © 2019 Eric Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
