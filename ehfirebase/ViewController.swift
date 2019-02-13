a9b21d79feb45b057bc6ef4daf81d0250c8e0572
Kyle Hernandez <kylehernandez@kyles-macbook-air.local>
2019-02-12 22:21:31 -0600

    add segue

--- ehfirebase/ViewController.swift
+++ ehfirebase/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class ViewController: UIViewController {
 
@@ -20,10 +21,33 @@ class ViewController: UIViewController {
     }
 
     @IBAction func loginBtn(_ sender: Any) {
+        SVProgressHUD.show()
         
+        Auth.auth().signIn(withEmail: userNameTxt.text!, password: passwordTxt.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("Log in successful!")
+                
+                SVProgressHUD.dismiss()
+                
+                self.performSegue(withIdentifier: "goToUpdateDB", sender: self)
+                
+            }
+            
+        }
     }
     @IBAction func registerBtn(_ sender: Any) {
-        Auth.auth().createUser(withEmail: <#T##String#>, password: <#T##String#>, completion: <#T##AuthDataResultCallback?##AuthDataResultCallback?##(AuthDataResult?, Error?) -> Void#>)
+        Auth.auth().createUser(withEmail: userNameTxt.text!, password: passwordTxt.text!){ (user, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                //success
+                print("Registration Successful!")
+            }
+        }
     }
     
 }
