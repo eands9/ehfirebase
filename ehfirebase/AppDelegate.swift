edb7d0e88bc282ebd71ecac05d132ea39e3dbf84
Kyle Hernandez <kylehernandez@kyles-macbook-air.local>
2019-02-15 20:49:40 -0600

    add go to background

--- ehfirebase/AppDelegate.swift
+++ ehfirebase/AppDelegate.swift
@@ -23,11 +23,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let userName1 = Auth.auth().currentUser?.email as! String
+        let modUserName11 = userName1.replacingOccurrences(of: "@", with: "")
+        let modUsername21 = modUserName11.replacingOccurrences(of: ".", with: "")
+        let improperlyClosedDB = Database.database().reference().child("Users").child(modUsername21)
+        //let refDictionary = ["AvgTime": 5 , "Date": getCurrentShortDate()] as [String : Any]
+        
+        
+        improperlyClosedDB.setValue(["ImproperlyClosed": "Y"]){
+            (error,reference) in
+            if error != nil{
+                print(error!)
+            } else {
+                print("Message saved successfully!")
+                
+            }
+        }
+
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
